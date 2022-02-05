daa27119abf11b84413ed19ed06846f0aecfb4fd
Jerry Uche <chubbyjerr@gmail.com>
2022-02-05 04:45:22 +0100

    Create Use getters and setters to Control Access to an Object.js
    Create the Class  Thermostat, Create the Farenheit constructor
    in the class create the getter that converts Celsius to Fahrenheit using the formula in the question
    create the setter that converts Fahrenheit  to Celsius using the formula

---
+++ Use getters and setters to Control Access to an Object.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+class Thermostat{
+  constructor(Fahrenheit) {
+    this.Fahrenheit = Fahrenheit;
+  }
+  //getter
+  get temperature () {
+    return (5 / 9 ) * ( this.Fahrenheit - 32);
+  }
+  // setter
+  set temperature (celsius) {
+    this.Fahrenheit = (celsius * 9.0) / 5 + 32 ;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
